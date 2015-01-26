6e897c579ec6dfb4301cd72b391709b1b9725303
jchernandezexpress <jchernandez@paquetexpress.com.mx>
2015-01-26 12:07:58 -0700

    Se agregan archivos png para los splash screen
    Se sube la app

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,45 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.toast/www/Toast.js",
+        "id": "nl.x-services.plugins.toast.Toast",
+        "clobbers": [
+            "window.plugins.toast"
+        ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.toast/test/tests.js",
+        "id": "nl.x-services.plugins.toast.tests"
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.dialogs": "0.2.11",
+    "org.apache.cordova.vibration": "0.3.12",
+    "nl.x-services.plugins.toast": "2.0.2"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/android/assets/www/js/personalizados/utiles.js
@@ -0,0 +1,115 @@
+/*Documento JS que contiene funciones necesarias
+ para el funcionamiento correcto de la aplicacion*/
+//Funcion que obtiene el ultimo evento de un rastreo
+function ultimoRastreo(rastreo){
+	var resultado = "";
+	var url= "http://192.168.10.45:7001/ptxws/rest/api/v1/guia/historico/ultimoevento/";
+	if(rastreo !=""){
+		$.ajax({
+				crossOrigin: true,
+				beforeSend: function() { $.mobile.loading( "show" ); },
+				type:"GET",
+				timeout:60000,
+				jsonp: "callback",
+				url: url+rastreo,
+				dataType: "jsonp",
+				jsonpCallback: 'Resultado',
+				success: function(datos) {
+					$.mobile.loading( "hide" );
+					$("#busqueda").empty();
+					var info = datos;
+					var respuesta ="";
+					if(info != ""){
+						$.each(datos, function(llave){
+							/*respuesta += "<li data-icon='carat-r-custom' id="+rastreo+">";
+							respuesta += "<a href='#'><img src='img/elements/rastreo-(caja).png' alt='' class='ui-li-icon'>";
+							respuesta += "<p class='ui-li-aside bg-green centered'><strong>" + datos[llave].status + " " + datos[llave].hora + "</strong></p>";
+							respuesta +="<span class='green'>"+rastreo+"</span></a></li>";*/
+							respuesta += "<li id="+rastreo+">";
+							respuesta += "<div class='card'>";
+							respuesta += "<p class='card-title'><strong>Guía: </strong>" + rastreo + "</p>";
+							respuesta += "<p><strong>" + datos[llave].status + "</strong></p>";
+							respuesta += "<p class='url'><a>" + datos[llave].fecha + " - " + datos[llave].hora + "</a></p>";
+							respuesta += "</li>";
+						});
+						$("#busqueda").append(respuesta).listview('refresh');
+						window.localStorage.setItem("rastreo", rastreo);
+					}
+					else{
+						window.plugins.toast.showLongBottom('!No se encontraron resultados!', function(a){
+							console.log('toast success: ' + a)}
+							, function(b){
+								alert('toast error: ' + b)});
+					}
+				},
+				error: function(jqXHR,text_status,strError){
+					$.mobile.loading( "hide" );
+					if(jqXHR.status == 404){
+						window.plugins.toast.showLongBottom('!Error 404: Por favor verifica tu conexión!');
+					}else if(jqXHR.status == 500){
+						window.plugins.toast.showLongBottom('!Error: 500\nEl servidor no esta listo!');
+					}
+					else{
+						alert("Error: "+ jqXHR.status + "\n"+jqXHR.error);
+					}
+				}
+			  });
+	}
+	else{
+		window.plugins.toast.showLongCenter('!Debes introducir un rastreo!');
+	}
+}
+
+//Funcion que obtiene el historial de un rastreo
+function historialRastreo(rastreo){
+	var resultado = "";
+	var url= "http://192.168.10.45:7001/ptxws/rest/api/v1/guia/historico/";
+	if(rastreo !=""){
+		$("#ul_historico").empty();
+		$.ajax({
+				crossOrigin: true,
+				beforeSend: function() { $.mobile.loading( "show" ); },
+				type:"GET",
+				timeout:60000,
+				jsonp: "callback",
+				url: url+rastreo,
+				dataType: "jsonp",
+				jsonpCallback: 'Resultado',
+				success: function(datos) {
+					var info = datos;
+					var respuesta ="";
+					if(info != ""){
+						$.each(datos, function(llave){
+						respuesta += "<li>";
+						respuesta += "<div class='card'>";
+						respuesta += "<p class='card-title'>" + datos[llave].sucursal + "</p>";
+						respuesta += "<p><strong>" + datos[llave].status + "</strong></p>";
+						respuesta += "<p class='url'><a>" + datos[llave].fecha + " - " + datos[llave].hora + "</a></p>";
+						respuesta += "</li>";
+					});
+					$("#ul_historico").append(respuesta);
+					$.mobile.changePage("#historial");
+					//Oculto el icono de cargando
+					$.mobile.loading( "hide" );
+					}
+					else{
+						window.plugins.toast.showLongBottom('!No se encontraron resultados!');
+					}
+				},
+				error: function(jqXHR,text_status,strError){
+					$.mobile.loading( "hide" );
+					if(jqXHR.status == 404){
+						window.plugins.toast.showLongBottom('!Error: 404\nPor vafor revisa tu conexión!');
+					}else if(jqXHR.status == 500){
+						window.plugins.toast.showLongBottom('!Error: 500\nEl servidor no esta listo!');
+					}
+					else{
+						alert("Error: "+ jqXHR.status + "\n"+jqXHR.error);
+					}
+				}
+			  });
+	}
+	else{
+		alert("No has ingresado numero de rastreo");
+	}
+}
